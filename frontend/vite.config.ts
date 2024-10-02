7d92a95d40d5505539161a939e7944a49fec012d
Arttu Haverinen <arttuhav@student.uef.fi>
2024-10-02 14:40:54 +0300

    docker files for frontend, docker-compose files

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,7 +1,14 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+	plugins: [react()],
+	server: {
+		host: "0.0.0.0", // Ensure it's set to listen on all interfaces
+		port: 5173, // This should match your port mapping
+		watch: {
+			usePolling: true, // Necessary for hot reloads inside Docker
+		},
+	},
+});
