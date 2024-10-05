a3b37875738f2534f9513bba06f16cfcf81de05d
Arttu Haverinen <arttuhav@student.uef.fi>
2024-10-05 20:58:59 +0300

    nginx related changes

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -4,6 +4,7 @@ import react from "@vitejs/plugin-react-swc";
 // https://vitejs.dev/config/
 export default defineConfig({
 	plugins: [react()],
+	//base: "/busapp/", // Base path for production
 	server: {
 		host: "0.0.0.0", // Ensure it's set to listen on all interfaces
 		port: 5173, // This should match your port mapping
