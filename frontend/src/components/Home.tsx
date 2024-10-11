7549702c87445f4a2bfa3f0e45640599ac97eee2
Arttu Haverinen <arttuhav@student.uef.fi>
2024-10-12 00:59:14 +0300

    workflow testing

--- frontend/src/components/Home.tsx
+++ frontend/src/components/Home.tsx
@@ -29,7 +29,7 @@ const Home = () => {
 			<Container className="w-100 image-wrapper">
 				<Image className="image-constrain-height" src={mountains} />
 			</Container>
-			<p>workflow 121024</p>
+			<p>workflow 121024 test</p>
 			<Search />
 			<Row className="w-100 my-5 mx-auto">
 				<Posts />
