c917f9fdac142fa018ec2b74727f4e94ff92e7e4
Arttu Haverinen <arttuhav@student.uef.fi>
2024-12-04 15:15:50 +0200

    send confirmation email after purchase and registeration, autoincrement db ids

---
+++ backend/Models/EmailSettings.cs
@@ -0,0 +1,12 @@
+﻿namespace TicketReservationApp.Models
+{
+    public class EmailSettings
+    {
+        public String SmtpServer { get; set; }
+        public int Port { get; set; }
+        public string SenderEmail { get; set; }
+        public string SenderName { get; set; }
+        public string Password { get; set; }
+
+    }
+}
