fdca60a42f680937ddfe8b57e86da5273aef4851
Arttu Haverinen <arttuhav@student.uef.fi>
2024-12-12 15:50:54 +0200

    update docker setup with frontend tests, additional dockerfiles

--- frontend/cypress/e2e/spec.cy.ts
+++ frontend/cypress/e2e/spec.cy.ts
@@ -1,6 +1,8 @@
 describe("template spec", () => {
 	beforeEach(() => {
-		cy.visit("http://localhost:3000/register");
+		//cy.visit("http://host.docker.internal:5173/register");
+		//cy.visit("http://localhost:5173/register");
+		cy.visit("http://test-client-vite:5173/register");
 	});
 	it("passes", () => {
 		//cy.get('[data-testid="register-username-label"]').type("");
