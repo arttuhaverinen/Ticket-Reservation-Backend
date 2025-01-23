7ff88ef594d62f1cea107a720d98a8e3128d99de
Arttu Haverinen <arttuhav@student.uef.fi>
2025-01-23 05:26:30 +0200

    try to fix e2e testing, 403 error

--- frontend/cypress/e2e/spec.cy.ts
+++ frontend/cypress/e2e/spec.cy.ts
@@ -1,5 +1,6 @@
 describe("template spec", () => {
 	beforeEach(() => {
+		cy.wait(5000); // Wait for 5 seconds to give Vite time to start
 		//cy.visit("http://host.docker.internal:5173/register");
 		//cy.visit("http://localhost:5173/register");
 		cy.visit("http://test-client-vite:5173/register", {
