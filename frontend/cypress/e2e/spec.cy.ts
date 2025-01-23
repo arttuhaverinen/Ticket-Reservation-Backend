90d032a2b92414fb826058093cea144c8fc88137
Arttu Haverinen <arttuhav@student.uef.fi>
2025-01-24 01:07:49 +0200

    fix cypress tests

--- frontend/cypress/e2e/spec.cy.ts
+++ frontend/cypress/e2e/spec.cy.ts
@@ -1,11 +1,9 @@
 describe("template spec", () => {
 	beforeEach(() => {
-		cy.wait(5000); // Wait for 5 seconds to give Vite time to start
+		//cy.wait(5000); // Wait for 5 seconds to give Vite time to start
 		//cy.visit("http://host.docker.internal:5173/register");
 		//cy.visit("http://localhost:5173/register");
-		cy.visit("http://test-client-vite:5173/register", {
-			failOnStatusCode: false,
-		});
+		cy.visit("http://test-client-vite:5173/register");
 	});
 	it("passes", () => {
 		//cy.get('[data-testid="register-username-label"]').type("");
