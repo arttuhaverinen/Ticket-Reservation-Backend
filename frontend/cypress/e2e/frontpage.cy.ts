7c55765f52bfc4d3f4647c597b17af5852faceea
Arttu Haverinen <arttuhav@student.uef.fi>
2025-02-05 02:12:30 +0200

    trying another e2e test workflow

--- frontend/cypress/e2e/frontpage.cy.ts
+++ frontend/cypress/e2e/frontpage.cy.ts
@@ -2,9 +2,8 @@ describe("template spec", () => {
 	beforeEach(() => {
 		//cy.wait(5000); // Wait for 5 seconds to give Vite time to start
 		//cy.visit("http://host.docker.internal:5173/register");
-		//cy.visit("http://localhost:3000");
+		cy.visit("/");
 
-		cy.visit("http://localhost:5173");
 		//cy.request("http://test-client-vite:5173").then((response) => {
 		//	console.log(response);
 		//});
