6f6a7f3a35d94fc95d2e57dadc0b933eb47e4085
Arttu Haverinen <arttuhav@student.uef.fi>
2025-02-06 18:50:22 +0200

    add custom refresh identity endpoint, that does not require access tokens

---
+++ backend/Controllers/AuthController.cs
@@ -0,0 +1,60 @@
+﻿namespace TicketReservationApp.Controllers;
+
+
+using Microsoft.AspNetCore.Authentication.BearerToken;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity.Data;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Options;
+
+using System.ComponentModel.DataAnnotations;
+using System.Diagnostics;
+using System.Linq;
+using System.Security.Claims;
+using System.Text;
+using System.Text.Encodings.Web;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Http.HttpResults;
+using Microsoft.AspNetCore.Http.Metadata;
+using Microsoft.AspNetCore.WebUtilities;
+using Microsoft.Extensions.DependencyInjection;
+using TicketReservationApp.Models;
+
+
+[ApiController]
+[Route("auth")]
+public class AuthController : ControllerBase
+{
+    private readonly SignInManager<AppUser> _signInManager;
+    private readonly IOptionsMonitor<BearerTokenOptions> _bearerTokenOptions;
+    private readonly TimeProvider _timeProvider;
+
+    public AuthController(
+        SignInManager<AppUser> signInManager,
+        IOptionsMonitor<BearerTokenOptions> bearerTokenOptions,
+        TimeProvider timeProvider)
+    {
+        _signInManager = signInManager;
+        _bearerTokenOptions = bearerTokenOptions;
+        _timeProvider = timeProvider;
+    }
+
+    [HttpPost("refresh")]
+    public async Task<IActionResult> Refresh([FromBody] RefreshRequest refreshRequest)
+    {
+        var refreshTokenProtector = _bearerTokenOptions.Get(IdentityConstants.BearerScheme).RefreshTokenProtector;
+        var refreshTicket = refreshTokenProtector.Unprotect(refreshRequest.RefreshToken);
+
+        if (refreshTicket?.Properties?.ExpiresUtc is not { } expiresUtc ||
+            _timeProvider.GetUtcNow() >= expiresUtc ||
+            await _signInManager.ValidateSecurityStampAsync(refreshTicket.Principal) is not AppUser user)
+        {
+            return Unauthorized();
+        }
+
+        var newPrincipal = await _signInManager.CreateUserPrincipalAsync(user);
+        return SignIn(newPrincipal, IdentityConstants.BearerScheme);
+    }
+}
